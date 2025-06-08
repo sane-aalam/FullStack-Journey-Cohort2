2b35ba9b5a9468e9e26acf2c550f14558b2a3838
Sane Aalam <saneraza78692@gmail.com>
2025-06-08 07:59:10 +0530

    issue: issues encountered during project setup with tailwind css

---
+++ 0-1/Week 5/Week 5.1/couter-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind + Vite!</h1>
+    </div>
+  );
+}
+
+export default App;
