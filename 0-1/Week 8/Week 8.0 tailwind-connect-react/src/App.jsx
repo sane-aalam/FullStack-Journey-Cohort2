f9dbe5963dafc3aefc5218ce2ca48e69ef089c08
Sane Aalam <saneraza78692@gmail.com>
2025-06-16 11:10:19 +0530

    feat: implement basic of tailwind-css

--- 0-1/Week 8/Week 8.0 tailwind-connect-react/src/App.jsx
+++ 0-1/Week 8/Week 8.0 tailwind-connect-react/src/App.jsx
@@ -1,10 +1,54 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
+
+const ContainerFirst = () =>{
   return(
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <div className='flex justify-around items-center w-full border-5 border-black p-4'>
+       <div className='bg-green-300 border-2 ml-5 p-2'>HOME</div>
+       <div className='bg-red-300 border-2 ml-5 p-2'>ABOUT</div>
+       <div className='bg-green-300 border-2 ml-5 p-2'>CONTACT US</div>
+       <div className='bg-green-300 border-2 ml-5 p-2'>CAREER PAGE</div>
+    </div>
+  )
+}
+
+const CardContainer = ({tittle}) =>{
+  return(
+    <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+      <div>
+        <img className="w-full" src='./img/pic1.jpg' alt="Sunset in the mountains"></img>
+      </div>
+      <div className='px-6 py-6'>
+        <div className="font-bold text-xl mb-2">{tittle}</div>
+        <div className='text-gray-700 text-base'>  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil. </div>
+      </div>
+      <div className='px-6 py-4'>
+        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>#photography</span>
+         <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>#photo</span>
+          <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>#tailwindcss</span>
+      </div>
+    </div>
+  )
+}
+
+const MainCardContainer = () =>{
+  return(
+    <div className='flex justify-center gap-4 ml-4 mr-4'>
+        <CardContainer tittle={"Water Hot"}/>
+        <CardContainer tittle={"Water Cool"}/>
+         <CardContainer tittle={"Water Cool"}/>
+        <CardContainer tittle={"Water Medium"}/>
+       </div>
+  )
+}
+
+
+function App() { 
+  return(
+    <h1 className="flex flex-col gap-4">
+       <ContainerFirst/>
+       <MainCardContainer/>
     </h1>
   )
 }
---
+++ 0-1/Week 8/Week 8.1 dukaan-deshboard/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ return(
+  <div style={{backgroundColor:"red"}}>
+      <div>dukaan deshboard 1</div>
+      <div>dukaan deshboard 2</div>
+      <div>dukaan deshboard 3</div>
+      <div>dukaan deshboard 4</div>
+  </div>
+ )
+}
+
+export default App
